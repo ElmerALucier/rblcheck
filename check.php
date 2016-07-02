b70a04bbc4b31704de9e68f132e073dde928ed0f
ElmerALucier <ElmerALucier@users.noreply.github.com>
2016-07-01 21:03:31 -0300

    Update check.php

--- check.php
+++ check.php
@@ -3,16 +3,18 @@
 
 require("rbl_lib.php");
 
-$blockedList = rblcheck($argv[1]);
-if(count($blockedList) > 0)
-{
-	// Your IP is blocked somewhere.
-	echo "IP is blocked on:\n";
-	print_r($blockedList);
+if(count($argv)>1){
+	$blockedList = rblcheck(trim($argv[1]));
+	if(count($blockedList) > 0)
+	{
+		// Your IP is blocked somewhere.
+		echo "IP is blocked on:\n";
+		print_r($blockedList);
+	}
+	else
+	{
+		echo "The IP is not blocked.";
+	}
+}else{
+	echo "usage: php ".$_SERVER['SCRIPT_NAME']. " <ip>";
 }
-else
-{
-	echo "The IP is not blocked.";
-}
-echo PHP_EOL;
-?>
